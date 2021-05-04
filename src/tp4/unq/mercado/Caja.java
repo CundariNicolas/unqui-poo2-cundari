74599f5a3aeb598c183bbe86f8a3ff7c02fc0550
Nicolás Cundari <nicolasmcundari@gmail.com>
2021-05-04 19:29:54 -0300

    Empezando Caja

---
+++ src/tp4/unq/mercado/Caja.java
@@ -0,0 +1,5 @@
+package tp4.unq.mercado;
+
+public class Caja {
+
+}
---
+++ test/tp4/unq/mercado/CajaTestCase.java
@@ -0,0 +1,19 @@
+package tp4.unq.mercado;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CajaTestCase {
+	
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
