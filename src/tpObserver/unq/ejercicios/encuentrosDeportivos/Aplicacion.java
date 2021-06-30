c6d14ada4a4ee7730a94e36cbfa394f983b281bd
Nicolás Cundari <nicolasmcundari@gmail.com>
2021-06-30 17:20:20 -0300

    Aplicacion

---
+++ src/tpObserver/unq/ejercicios/encuentrosDeportivos/Aplicacion.java
@@ -0,0 +1,5 @@
+package tpObserver.unq.ejercicios.encuentrosDeportivos;
+
+public class Aplicacion {
+
+}
--- src/tpObserver/unq/ejercicios/publicaciones/Articulo.java
+++ src/tpObserver/unq/ejercicios/publicaciones/Articulo.java
@@ -27,7 +27,7 @@ public class Articulo {
 
 
 	public String getTitulo() {
-		return titulo;
+		return titulo; 
 	}
 
 
